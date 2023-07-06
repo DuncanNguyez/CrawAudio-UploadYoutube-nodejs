680c7edbf97c20d2888faeb8ca6bb67e8c837a6b
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-06 20:11:51 +0700

    Refactor: use fluent-ffmpeg library instead of videoshow

--- src/modules/convertToVideo/index.js
+++ src/modules/convertToVideo/index.js
@@ -1,26 +1,25 @@
-import getAudioDurationInSeconds from 'get-audio-duration';
-import videoshow from 'videoshow';
+const ffmpeg = require('fluent-ffmpeg');
 
 export default async (imagesPath, audioPath, videoPath) => {
-    const videoOptions = {
-        fps: 1,
-        loop: await getAudioDurationInSeconds(audioPath), // seconds
-        videoBitrate: 250,
-        videoCodec: 'mpeg4',
-        size: '640x?',
-        audioBitrate: '128k',
-        audioChannels: 2,
-        format: 'mp4',
-        pixelFormat: 'nv21',
-    };
+    const command = ffmpeg();
+
     return new Promise((resolve, reject) => {
-        videoshow(imagesPath, videoOptions)
-            .audio(audioPath)
-            .save(videoPath)
-            .on('start', function (command) {
-                console.log('ffmpeg process started:', command);
-            })
+        command
+            .input(imagesPath)
+            .input(audioPath)
+            .audioFilters('atempo=1.2')
+
+            // output configuration options
+            .outputOptions('-b:a 128k')
+            .outputOptions('-b:v 250')
+            .outputOptions('-r 1')
+            .outputOptions('-pix_fmt nv21')
+            .outputOptions('-c:v mpeg4')
+            .outputOptions('-crf 30')
+
+            .output(videoPath)
+            .on('error', reject)
             .on('end', resolve)
-            .on('error', reject);
+            .run();
     });
 };
