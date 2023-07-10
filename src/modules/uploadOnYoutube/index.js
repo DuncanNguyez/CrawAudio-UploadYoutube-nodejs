c46ce6fac49af8f83f5b1d179bc8f890b2f89318
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-10 13:14:30 +0700

    Style: export from index file

---
+++ src/modules/uploadOnYoutube/index.js
@@ -0,0 +1,15 @@
+import getChannelSection from './getChannelSection.js';
+import insertChannelSection from './insertChannelSection.js';
+import insertPlaylist from './insertPlaylist';
+import insertPlaylistItem from './insertPlaylistItem';
+import insertVideo from './insertVideo';
+import getAuth from './getAuth.js';
+
+export {
+    insertPlaylist,
+    insertVideo,
+    insertPlaylistItem,
+    insertChannelSection,
+    getChannelSection,
+    getAuth,
+};
