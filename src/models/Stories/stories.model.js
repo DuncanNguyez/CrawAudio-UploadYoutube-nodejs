ee4af1302882eb6ce36bdf05c84d1ac1beb78771
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-10 20:33:53 +0700

    Refacter: set default status of item

--- src/models/Stories/stories.model.js
+++ src/models/Stories/stories.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 const item = new Schema({
     audioUrl: { type: String, required: true },
     episode: { type: Number, required: true },
-    status: Boolean,
+    status: { type: Boolean, default: false },
     youtubeUrl: String,
     youtubeId: String,
 });
