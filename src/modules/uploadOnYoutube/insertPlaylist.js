0fae01c5bc180a40de6d3082532847ff9a50095d
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-13 01:38:50 +0700

    Refactor: changed status when inseting  playlist into  youtube

--- src/modules/uploadOnYoutube/insertPlaylist.js
+++ src/modules/uploadOnYoutube/insertPlaylist.js
@@ -11,7 +11,7 @@ export default async ({ auth, title, description, tags }) => {
                 description,
                 tags,
             },
-            status: { privacyStatus: 'private' },
+            status: { privacyStatus: 'public' },
         },
     });
 };
