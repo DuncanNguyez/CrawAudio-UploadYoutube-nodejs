c7f8f01efd262bb01b7be58b0772da0ffc32a940
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-12 20:04:22 +0700

    Refactor: edit option when converting video

--- src/modules/convertToVideo/index.js
+++ src/modules/convertToVideo/index.js
@@ -1,26 +1,25 @@
 import ffmpeg from 'fluent-ffmpeg';
+import { getAudioDurationInSeconds } from 'get-audio-duration';
 
 export default async ({ imagesPath, audioPath, videoPath }) => {
     const command = ffmpeg();
-
+    const loop = await getAudioDurationInSeconds(audioPath);
     return new Promise((resolve, reject) => {
         command
-            .input(imagesPath)
-            .input(audioPath)
-            .audioFilters('atempo=1.2')
 
-            // output configuration options
-            .outputOptions('-b:a 128k')
-            .outputOptions('-b:v 250')
-            .outputOptions('-r 1')
-            .outputOptions('-pix_fmt nv21')
-            .outputOptions('-c:v mpeg4')
-            .outputOptions('-crf 30')
-            .toFormat('mp4')
+            .input(imagesPath)
+            .inputFPS(1)
+            .loop(loop / 1.2)
+            .input(audioPath)
+            .complexFilter(['atempo=1.2', 'scale=w=640:h=trunc(ow/a/2)*2'])
+            .videoCodec('libx264')
+            .audioCodec('aac')
+            .outputOptions(['-b:v 1024k', '-b:a 128k', '-ac 2', '-r 1'])
 
             .output(videoPath)
             .on('error', reject)
             .on('end', resolve)
+            // .on('start', (cmd) => console.log(cmd))
             .run();
     });
 };
