c25b17157a3aded27c9955663e27d50fc9c15338
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-13 23:04:59 +0700

    Fix: Not connected to the database

--- scripts/crawling.js
+++ scripts/crawling.js
@@ -1,5 +1,7 @@
 import { crawInfoDefaultCron, crawInfoPageCron } from '../src/cronJob/index.js';
+import { connectDB } from '../src/utils/index.js';
 
+await connectDB();
 await crawInfoDefaultCron();
 await crawInfoPageCron();
 
--- scripts/uploading.js
+++ scripts/uploading.js
@@ -1,4 +1,6 @@
 import { uploadToYoutubeCron } from '../src/cronJob/index.js';
+import { connectDB } from '../src/utils/index.js';
 
+await connectDB();
 await uploadToYoutubeCron();
 process.exit();
