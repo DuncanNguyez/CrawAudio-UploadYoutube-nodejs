d064d29602a1ce197fe0ca11a7c699886291b2f3
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-13 11:40:15 +0700

    Style: use rmSync method

--- src/utils/clearFolder.js
+++ src/utils/clearFolder.js
@@ -1,19 +1,8 @@
 import fs from 'fs';
-import path from 'path';
 
-const clearFolder = async (fPath) => {
+const clearFolder = async (path) => {
     try {
-        if (fs.lstatSync(fPath).isDirectory()) {
-            if (fs.existsSync(fPath)) {
-                fs.readdirSync(fPath).forEach((file) => {
-                    const curPath = path.join(fPath, file);
-                    clearFolder(curPath);
-                });
-                fs.rmdirSync(fPath);
-            }
-        } else {
-            fs.unlinkSync(fPath);
-        }
+        fs.rmSync(path);
         return true;
     } catch (error) {
         console.error(error.message);
