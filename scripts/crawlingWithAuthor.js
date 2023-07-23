186a22e43bbc1b44dfb8d08564f449e2aa2dbc89
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-07-23 18:12:17 +0700

    Refactor: run script with readline

--- scripts/crawlingWithAuthor.js
+++ scripts/crawlingWithAuthor.js
@@ -1,10 +1,20 @@
+import readline from 'readline';
+
 import { handleCrawlInfo } from '../src/cronJob/services/index.js';
 import { Authors } from '../src/models/index.js';
 import { connectDB } from '../src/utils/index.js';
 
 await connectDB();
-const authorId = process.argv[2];
-const name = process.argv[3];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const authorId = await new Promise((resolve) =>
+    rl.question('authorId', resolve)
+);
+const name = await new Promise((resolve) => rl.question('authorName', resolve));
+
 const url = `https://audiotruyenfull.com/tac-gia/${authorId}/?stt=hoan-thanh`;
 const crawlAuthorPage = await handleCrawlInfo(url);
 if (crawlAuthorPage) {
--- scripts/uploadingWithStory.js
+++ scripts/uploadingWithStory.js
@@ -1,11 +1,16 @@
+import readline from 'readline';
 import { handleUpload } from '../src/cronJob/services/index.js';
 import { Stories } from '../src/models/index.js';
 import { connectDB } from '../src/utils/index.js';
 
 await connectDB();
-const id = process.argv[2];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const id = await new Promise((resolve) => rl.question('storyId', resolve));
+
 const stories = await Stories.findOne({ id }).lean();
-console.log(stories);
 await handleUpload(stories);
 
 process.exit();
