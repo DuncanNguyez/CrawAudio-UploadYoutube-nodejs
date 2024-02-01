9fef5d4691b9bda22eac3c579ee6884bb0af387e
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-02-01 18:23:07 +0700

    Move db to could

--- src/utils/connectDB/index.js
+++ src/utils/connectDB/index.js
@@ -1,11 +1,26 @@
 import config from 'config';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const { port, name } = config.get('database');
+const {
+    MONGODB_HOST: host,
+    MONGODB_USERNAME: username,
+    MONGODB_PASSWORD: password,
+    NODE_ENV: env,
+} = process.env;
 
 export default async () => {
-    const url = `mongodb://localhost:${port}/${name}`;
-    await mongoose.connect(url);
-    console.log(`Successfully connected to MongoDb on ${port}`);
+    const uri =
+        env == 'production'
+            ? `mongodb+srv://${username}:${password}@${host}/${name}`
+            : `mongodb://localhost:${port}/${name}`;
+    const cnn = await mongoose.connect(uri);
+
+    console.log(
+        `Successfully connected to MongoDb on ${cnn.connections[0].port}`
+    );
     console.log(`DbName: ${name}`);
 };
